015fcb1dcfc224f0f3b70c7874edd644acf23944
someshdeshmukh <108639380+someshdeshmukh@users.noreply.github.com>
2023-04-28 19:44:13 +0530

    some changes

--- LinkedListOperations/sizeOperation.java
+++ LinkedListOperations/sizeOperation.java
@@ -5,8 +5,8 @@ import java.util.LinkedList;
 public class sizeOperation {
     public static void main(String[] args){
         LinkedList<String> list = new LinkedList<>();
-        list.add("Geeks for Geeks");
-        list.add("is best");
+        list.add("Deshukh Somesh is ");
+        list.add("my name");
         System.out.println("The size of LinkedList is :" + list.size());
     }
 }
